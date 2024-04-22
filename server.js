e6dff398276e3de60e1bba0f258e71065c6e6dcf
kvora2 <kelvinvora21@gmail.com>
2024-04-22 18:50:34 -0400

    Update JWT token generation

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const HTTP_PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-
-let token = jwt.sign({ userName: 'bob' }, 'secret');
+// eg
+// let token = jwt.sign({ userName: 'bob' }, 'secret');
 
 // JSON Web Token Setup
 let ExtractJwt = passportJWT.ExtractJwt;
